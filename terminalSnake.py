b7cfd4b315b89a933525ed58139463747fef305b
Kieran Nageotte <30636961+kierannageotte@users.noreply.github.com>
2021-11-19 00:43:47 -0800

    adds random dot function

--- terminalSnake.py
+++ terminalSnake.py
@@ -1,3 +1,5 @@
+import random
+
 print("hi")
 board_size = int(input("Input number of cells on one side: "))
 print(board_size**2)
@@ -6,7 +8,23 @@ possible_actions = ["north", "south", "east", "west", "", "quit"]
 
 active_cells = [[0,0]]
 direction = "east"
+vector = [1,0]
 action = ""
+
+def make_dot():
+    dot = [random.randint(0,board_size-1),random.randint(0,board_size-1)]
+    while(dot in active_cells):
+        dot = [random.randint(0,board_size-1),random.randint(0,board_size-1)]
+    return dot
+
+def add_vect(a = [], b = []):
+    a[0] += b[0]
+    a[1] += b[1]
+    return a
+
+dot = make_dot()
+
+#Main Gameplay Loop
 while action != "quit":
     #get user input
     while True:
@@ -21,6 +39,7 @@ while action != "quit":
             direction = action
             break
 
+    #Execute actions
     if direction == "east":
         active_cells[0][0] += 1
     elif direction == "west":
@@ -30,4 +49,6 @@ while action != "quit":
     elif direction == "south":
         active_cells[0][1] -= 1
 
+#    if active_cells[0] == dot:
+#       active_cells +=
     print(active_cells)
