960a83cfe7ad10ef6b4435bb25d2a1b58aa54b84
Kieran Nageotte <30636961+kierannageotte@users.noreply.github.com>
2021-11-27 16:34:24 -0800

    Adds lots of functionality

--- terminalSnake.py
+++ terminalSnake.py
@@ -6,7 +6,10 @@ print(board_size**2)
 
 possible_actions = ["north", "south", "east", "west", "", "quit"]
 
+num_active = 1
+cell_history = [[0,1]]
 active_cells = [[0,0]]
+last_cell = [-1,0]
 direction = "east"
 vector = [1,0]
 action = ""
@@ -22,10 +25,24 @@ def add_vect(a = [], b = []):
     a[1] += b[1]
     return a
 
+def store_cells():
+    for i in range(len(active_cells)):
+        cell_history[i][0] = active_cells[i][0]
+        cell_history[i][1] = active_cells[i][1]
+
 dot = make_dot()
+
+def store_cell():
+    last_cell[0] = active_cells[-1][0]
+    last_cell[1] = active_cells[-1][1]
+    print("Stored; last cell = " + str(last_cell))
 
 #Main Gameplay Loop
 while action != "quit":
+    print("\n\nbegin loop")
+    print("Current active cells: " + str(active_cells))
+    print("Dot: " + str(dot))
+
     #get user input
     while True:
         action = input("What do? ")
@@ -39,7 +56,15 @@ while action != "quit":
             direction = action
             break
 
-    #Execute actions
+    #Store current cell
+    store_cell()
+    #print("Type: " + str(type(active_cells[0][0])))
+
+    # Execute move;
+    # First, "shift" all cells right one
+    for i in range(len(active_cells) - 1):
+        active_cells[i+1] = active_cells[i]
+    # Then, change value of [0] cell
     if direction == "east":
         active_cells[0][0] += 1
     elif direction == "west":
@@ -49,6 +74,10 @@ while action != "quit":
     elif direction == "south":
         active_cells[0][1] -= 1
 
-#    if active_cells[0] == dot:
-#       active_cells +=
-    print(active_cells)
+    #print("cell_history: " + str(cell_history))
+
+    if active_cells[0] == dot:
+        #print("cell history 2: " + str(cell_history))
+        active_cells.append(last_cell)
+        #cell_history.append(active_cells[0])
+        dot = make_dot()
