01b495542d0b43c588b8b672402696a610623b08
Kieran Nageotte <30636961+kierannageotte@users.noreply.github.com>
2021-11-28 13:55:03 -0800

    Corrects list issue; point lists now tuples

--- terminalSnake.py
+++ terminalSnake.py
@@ -6,36 +6,28 @@ print(board_size**2)
 
 possible_actions = ["north", "south", "east", "west", "", "quit"]
 
-num_active = 1
-cell_history = [[0,1]]
-active_cells = [[0,0]]
-last_cell = [-1,0]
+active_cells = [(0,0)]
 direction = "east"
-vector = [1,0]
 action = ""
 
+def add_tuple(a,b):
+    lst = list(a)
+    for i in range(len(lst)):
+        lst[i] += b[i]
+    return tuple(lst)
+
 def make_dot():
-    dot = [random.randint(0,board_size-1),random.randint(0,board_size-1)]
+    dot = (random.randint(0,board_size-1),random.randint(0,board_size-1))
     while(dot in active_cells):
-        dot = [random.randint(0,board_size-1),random.randint(0,board_size-1)]
+        dot = (random.randint(0,board_size-1),random.randint(0,board_size-1))
     return dot
 
-def add_vect(a = [], b = []):
-    a[0] += b[0]
-    a[1] += b[1]
-    return a
+hist1 = (0,1)
+hist2 = (0,2)
+hist3 = (0,3)
 
-def store_cells():
-    for i in range(len(active_cells)):
-        cell_history[i][0] = active_cells[i][0]
-        cell_history[i][1] = active_cells[i][1]
-
-dot = make_dot()
-
-def store_cell():
-    last_cell[0] = active_cells[-1][0]
-    last_cell[1] = active_cells[-1][1]
-    print("Stored; last cell = " + str(last_cell))
+#dot = make_dot()
+dot = (active_cells[0][0] + 2, 0)
 
 #Main Gameplay Loop
 while action != "quit":
@@ -56,28 +48,38 @@ while action != "quit":
             direction = action
             break
 
-    #Store current cell
-    store_cell()
-    #print("Type: " + str(type(active_cells[0][0])))
+    #Store current cells
+    hist3 = hist2
+    hist2 = hist1
+    print(hist1)
+    hist1 = active_cells[-1]
+    #store_cell()
 
     # Execute move;
     # First, "shift" all cells right one
-    for i in range(len(active_cells) - 1):
+    for i in reversed(range(len(active_cells) - 1)):
         active_cells[i+1] = active_cells[i]
+
     # Then, change value of [0] cell
     if direction == "east":
-        active_cells[0][0] += 1
+        #active_cells[0][0] += 1
+        active_cells[0] = add_tuple(active_cells[0], (1,0))
     elif direction == "west":
-        active_cells[0][0] -= 1
+        #active_cells[0][0] -= 1
+        active_cells[0] = add_tuple(active_cells[0], (-1, 0))
     elif direction == "north":
-        active_cells[0][1] += 1
+        #active_cells[0][1] += 1
+        active_cells[0] = add_tuple(active_cells[0], (0, 1))
     elif direction == "south":
-        active_cells[0][1] -= 1
+        #active_cells[0][1] -= 1
+        active_cells[0] = add_tuple(active_cells[0], (0, -1))
 
     #print("cell_history: " + str(cell_history))
 
     if active_cells[0] == dot:
         #print("cell history 2: " + str(cell_history))
-        active_cells.append(last_cell)
-        #cell_history.append(active_cells[0])
-        dot = make_dot()
+        active_cells.append(hist1)
+        hist1 = hist2
+        hist2 = hist3
+        #dot = make_dot()
+        dot = (active_cells[0][0] + 2, 0)
